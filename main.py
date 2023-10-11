103aed221ebb64f786e3d17bb210ab1e0852152b
Han Yu <97258094+BFLion@users.noreply.github.com>
2023-10-11 11:44:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,29 +18,27 @@ class DocumentSearch:
             line = line.strip()
             if not line:
                 continue
-
-            if "章节" in line:
-                self.chapters.append(self.Entry())
-                currentChapter = self.chapters[-1]
+            
+            if line.count(".") == 1:
+                currentChapter = self.Entry()
+                self.chapters.append(currentChapter)
                 currentSubChapter = None
                 currentChapter.title = line
-            elif "." in line and line.count(".") == 1:
-                if currentChapter:
-                    currentChapter.subEntries.append(self.Entry())
-                    currentSubChapter = currentChapter.subEntries[-1]
-                    currentSubChapter.title = line
-            elif line.count(".") > 1:
+            elif line.count(".") == 2:
+                currentSubChapter = self.Entry()
+                currentChapter.subEntries.append(currentSubChapter)
+                currentSubChapter.title = line
+            elif "、" in line and "？" in line:  
                 if currentSubChapter:
                     currentSubChapter.subEntries.append(self.Entry())
                     currentSubChapter.subEntries[-1].title = line
             else:
-                if currentChapter and not currentSubChapter:
-                    currentChapter.content += line + "\n"
+                if currentSubChapter and currentSubChapter.subEntries:
+                    currentSubChapter.subEntries[-1].content += line + "\n"
                 elif currentSubChapter:
-                    if not currentSubChapter.subEntries:
-                        currentSubChapter.content += line + "\n"
-                    else:
-                        currentSubChapter.subEntries[-1].content += line + "\n"
+                    currentSubChapter.content += line + "\n"
+                elif currentChapter:
+                    currentChapter.content += line + "\n"
 
     def searchByKeyword(self, keyword):
         foundSections = []
@@ -49,32 +47,32 @@ class DocumentSearch:
             for subChapter in chapter.subEntries:
                 for subSubChapter in subChapter.subEntries:
                     if keyword in subSubChapter.title or keyword in subSubChapter.content:
-                        first_line_content = subSubChapter.content.split('\n')[0]
-                        result = f"{chapter.title.split(' ')[0]} > {subChapter.title.split(' ')[0]} > {subSubChapter.title} \"{first_line_content}\""
+                        result = f"{chapter.title.split(' ')[0]} > {subChapter.title.split(' ')[0]} > {subSubChapter.title}{subSubChapter.content}"
                         foundSections.append(result)
 
                 if keyword in subChapter.title or keyword in subChapter.content:
-                    first_line_content = subChapter.content.split('\n')[0]
-                    result = f"{chapter.title.split(' ')[0]} > {subChapter.title} \"{first_line_content}\""
+                
+                    if not subChapter.subEntries:
+                        result = f"{chapter.title.split(' ')[0]} > {subChapter.title.split(' ')[0]}、{subChapter.title}{subChapter.content}"
+                    else:
+                        result = f"{chapter.title.split(' ')[0]} > {subChapter.title}{subChapter.content}"
                     foundSections.append(result)
 
             if keyword in chapter.title or keyword in chapter.content:
-                first_line_content = chapter.content.split('\n')[0]
-                result = f"{chapter.title} \"{first_line_content}\""
+                result = f"{chapter.title}{chapter.content}"
                 foundSections.append(result)
 
         return foundSections
 
-
 if __name__ == "__main__":
     ds = DocumentSearch()
-    #ds.loadDocument("sample.txt")
     ds.loadDocument("handbook.txt")
+    #ds.loadDocument("sample.txt")
     keyword = input("您想查询的关键词: ")
 
     sections = ds.searchByKeyword(keyword)
     if not sections:
-        print("没有结果!")
+        print("没有结果!") 
     else:
         print("关键词出现在以下章节：")
         for section in sections:
